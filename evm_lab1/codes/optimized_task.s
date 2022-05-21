a2b5a9436af6b51c5f675736802570e9e91464b3
Mikhail Geine <mike.geine@gmail.com>
2022-05-21 16:48:55 +0300

    Sem7 EVM Lab1

---
+++ evm_lab1/codes/optimized_task.s
@@ -0,0 +1,29 @@
+        .section .text
+        .globl _start;
+        len = 8 #Размер массива
+        enroll = 1 #Количество обрабатываемых элементов за одну итерацию
+	elem_sz = 4 #Размер одного элемента массива
+
+_start:
+        la x1, _x
+        addi x20, x1, elem_sz*(len-1) #Адрес последнего элемента
+
+		lw x2, 0(x1)
+lp:
+        addi x1, x1, elem_sz*enroll
+        add x31, x31, x2 #!
+        
+		lw x2, 0(x1)
+        bne x1, x20, lp
+        addi x31, x31, 1
+lp2: j lp2
+
+        .section .data
+_x:     .4byte 0x1
+        .4byte 0x2
+        .4byte 0x3
+        .4byte 0x4
+        .4byte 0x5
+        .4byte 0x6
+        .4byte 0x7
+        .4byte 0x8
